5f7db04f665627f48b4f782d38496520cd3ef078
Murilo Aires <muriloaires1@gmail.com>
2018-05-03 12:31:22 -0300

    Transfering project from bitbucker to github

---
+++ app/src/main/java/brmobi/moop/utils/TextUtils.java
@@ -0,0 +1,16 @@
+package brmobi.moop.utils
+
+internal class TextUtils {
+    var `object`: companion? = null
+
+    init {
+        val validEmail: `fun`
+        email
+        String
+        Boolean
+        run {
+            val pattern = Patterns.EMAIL_ADDRESS
+            return pattern.matcher(email).matches()
+        }
+    }
+}
